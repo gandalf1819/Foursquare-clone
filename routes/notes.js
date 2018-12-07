d1320ffe894f0a18bf0558a09865a48b1b3d83ed
Chinmay Wyawahare <chinmaynw@gmail.com>
2018-12-06 23:39:33 -0500

    notes route for notes controller

---
+++ routes/notes.js
@@ -0,0 +1,37 @@
+import {
+  notes as notesController
+} from '../controllers';
+
+import{
+  fetchToken
+}
+from '../middlewares';
+
+var Message = require('../util')
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/', fetchToken, function(req, res, next) {
+  console.log("req====", req);
+  notesController.createNotes(req.body, req.user_details)
+    .then(msg => {
+      res.send(Message.generateMessage(200, {}, msg));
+    })
+    .catch(msg => {
+      res.send(Message.generateMessage(422, {}, msg));
+    })
+});
+
+router.get('/', fetchToken, function(req, res, next) {
+  console.log("req====", req);
+  notesController.getNotes(req.query.filter_id, req.user_details)
+    .then(data => {
+      res.send(Message.generateMessage(200, data, "Notes filtered Successfully!!"));
+    })
+    .catch(msg => {
+      res.send(Message.generateMessage(422, {}, msg));
+    })
+});
+
+module.exports = router;
