62b9f8f6225f1dcf0cf9441ba1d36e0dd49ab62f
Chinmay Wyawahare <chinmaynw@gmail.com>
2018-12-06 23:28:23 -0500

    updated index file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+import auth from './auth'
+import notes from './notes'
+import filters from './filters'
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'} );
 });
+
+router.get('/login/', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/posts/', function(req, res, next) {
+  res.render('posts');
+});
+
+router.use('/auth/', auth)
+router.use('/notes/', notes)
+router.use('/filters/', filters)
 
 module.exports = router;
