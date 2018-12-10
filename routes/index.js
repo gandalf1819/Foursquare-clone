2eaed8ce682f81f46785b9d8a7eaaebf2059cd9e
Chinmay Wyawahare <chinmaynw@gmail.com>
2018-12-10 14:58:05 -0500

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import auth from './auth'
 import notes from './notes'
 import filters from './filters'
+import users from './users'
+
 var express = require('express');
 var router = express.Router();
 
@@ -17,13 +19,13 @@ router.get('/posts/', function(req, res, next) {
   res.render('posts', { friends : 'Express'} );
 });
 
-/*router.get('/friends', function(req, res, next) {
-  res.render('friends', { friends } );
-})*/
+router.get('/friends', function(req, res, next) {
+  res.render('friends', { friends: 'Express' } );
+});
 
 router.use('/auth/', auth)
 router.use('/notes/', notes)
 router.use('/filters/', filters)
-
+router.use('/users', users)
 module.exports = router;
 
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,7 @@
+import
+{users as usersController}
+from "../controllers";
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +9,30 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/friends', function(req, res, next) {
+    console.log("req====", req.cookies);
+    let accessToken="";
+    if(req.cookies['x-access-token']){
+        accessToken = req.cookies['x-access-token'];
+    }
+
+    let data = {
+        "first_name": req.user.first_name
+    }
+
+    usersController.friends(data)
+        .then(token => {
+            res.cookie('x-access-token', token)
+            let data = {
+                "x-access-token": token
+            }
+
+            res.send(Message.generateMessage(200, data, "Friend List fetch successfully!"));
+        })
+        .catch(msg => {
+            res.send(Message.generateMessage(422, {}, msg));
+        })
+})
 
 module.exports = router;
